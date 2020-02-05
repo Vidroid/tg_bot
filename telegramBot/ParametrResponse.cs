1e2ccca47ef99ec97b45c72bc5b4ffb3221c1e8f
Vidroid <frezerusha@gmail.com>
2020-02-05 12:01:41 +0300

    Начальная стадия. Инициализация репозитория. Создан основной набор методов Телеграм. Заколхожен метод гет апдейтс. ТуДу: энтити для бд, контроллеры для обработки команд ТуДу: роутер, логика управления запросами, распаралеливание, парсинг

---
+++ telegramBot/ParametrResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace telegramBot
+{
+    class ParametrResponse : EventArgs
+    {
+        public string name="";
+        public string username="";
+        public string messageText="";
+        public long chatId = 0;
+        public long fromId = 0;
+        public long replyUser = 0;
+        public int replyMessage = 0;
+        public int messageId = 0;
+
+
+    }
+}
---
+++ telegramBot/TelegramMethods.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Collections.Specialized;
+
+namespace telegramBot
+{
+    class TelegramMethods
+    {
+        private string _token;
+        private string _url = "https://api.telegram.org/bot";
+        public TelegramMethods(string Token)
+        {
+            this._token = Token;
+        }
+
+        public string GetMe()
+        {
+            using (var webClient = new WebClient())
+            {
+                return webClient.DownloadString(_url+_token+"/getMe");
+            }
+        }
+
+        public void SendMessage(string message, long id)
+        {
+            var pars = new NameValueCollection();
+            pars.Add("text", message);
+            pars.Add("chat_id", id.ToString());
+            using (var webClient = new WebClient())
+            {
+                webClient.UploadValues(_url + _token + "/sendMessage", pars);
+            }
+        }
+
+        public void EditMessage(string message, long id, long messageId)
+        {
+            var pars = new NameValueCollection();
+            pars.Add("text", message);
+            pars.Add("message_id", messageId.ToString());
+            pars.Add("chat_id", id.ToString());
+            using (var webClient = new WebClient())
+            {
+                webClient.UploadValues(_url + _token + "/editMessage", pars);
+            }
+        }
+
+        public void PinMessage(long id, long messageId)
+        {
+            var pars = new NameValueCollection();
+            pars.Add("disable_notification", false.ToString());
+            pars.Add("message_id", messageId.ToString());
+            pars.Add("chat_id", id.ToString());
+            using (var webClient = new WebClient())
+            {
+                webClient.UploadValues(_url + _token + "/pinChatMessage", pars);
+            }
+        }
+
+        public void UnPinMessage(long id)
+        {
+            var pars = new NameValueCollection();
+            pars.Add("chat_id", id.ToString());
+            using (var webClient = new WebClient())
+            {
+                webClient.UploadValues(_url + _token + "/unpinChatMessage", pars);
+            }
+        }
+
+    }
+}
