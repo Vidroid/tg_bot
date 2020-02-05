0cdccbd25ca44427cccbe738f8626627f765b2cc
Vidroid <frezerusha@gmail.com>
2020-02-05 14:17:01 +0300

    Refactored main function. Method GetUpdates moved to TelegramMetgods class. Method OnResponse moved to router class.

--- telegramBot/Router.cs
+++ telegramBot/Router.cs
@@ -7,5 +7,14 @@ namespace telegramBot
 {
     class Router
     {
+        public static void OnResponse(object sender, ParametrResponse e)
+        {
+
+            if (e.messageText == "/olo")
+            {
+                // tM.PinMessage(e.chatId, e.messageId);
+            }
+            Console.WriteLine("{0}: {1}", e.chatId, e.messageText);
+        }
     }
 }
